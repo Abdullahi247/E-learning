07df97c1b9e189708994f5cba122b36783580e36
Abdullahi247 <tsmart247@gmail.com>
2024-07-29 16:06:51 +0100

    dev

--- pages/bank/individual.jsx
+++ pages/bank/individual.jsx
@@ -8,9 +8,9 @@ import React from 'react'
 export default function BankForm () {
   return (
     <>
-      {/* <ParticlesComponent /> */}
-      {/* <div style={{ height:'fit-content' }}> */}
-      <div style={{ backgroundColor: '#518EDD', height:'fit-content' }}>
+      <ParticlesComponent />
+      <div style={{ height:'fit-content' }}>
+      {/* <div style={{ backgroundColor: '#518EDD', height:'fit-content' }}> */}
         <BankFormContainer>
           <div className='container'>
             <IndividualBankformsHeader />
@@ -20,7 +20,7 @@ export default function BankForm () {
           </div>
         </BankFormContainer>
       </div>
-      {/* <IndividualBankFooter /> */}
+      <IndividualBankFooter />
     </>
   )
 }
